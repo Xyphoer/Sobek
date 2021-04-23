0434c23c71cea59e45e01039982c0e8d04fd940d
Xyphoer <pizzacrime3000@gmail.com>
2021-04-23 18:50:56 -0500

    added purge command

--- cogs/mod.py
+++ cogs/mod.py
@@ -1,16 +1,17 @@
 import discord
 from discord.ext import commands
 from .utils import checks
+from typing import Optional
 
 class Mod(commands.Cog):
     """Moderation commands exclusive to Officers and Admins"""
 
-    def __init__(self):
+    def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     @checks.is_officer()
-    async def kick(self, ctx, members: commands.Greedy[discord.Member] = [], *reason = None):
+    async def kick(self, ctx, members: commands.Greedy[discord.Member] = [], *, reason = None):
         """
         Kicks the specified member(s) for the optional reason.
 
@@ -32,7 +33,7 @@ class Mod(commands.Cog):
 
     @commands.command()
     @checks.is_officer()
-    async def ban(self, ctx, members: commands.Greedy[discord.Member] = [], purge_days: int = 0, *reason = None):
+    async def ban(self, ctx, members: commands.Greedy[discord.Member] = [], purge_days: int = 0, *, reason = None):
         """
         Bans the specified member(s) for the optional reason, purging messages from the amount of days specified (max 7).
 
@@ -52,3 +53,29 @@ class Mod(commands.Cog):
             total.append(f'{member.name}({member.id})')
         if total: await ctx.send(f'Banned {", ".join(total)}. Purged {purge_days if purge_days <= 7 else 7} days of messages. Reason: {reason}')
         else: await ctx.send('Specify a member to ban.')
+
+    @commands.command()
+    @checks.is_officer()
+    async def purge(self, ctx, members: commands.Greedy[discord.Member] = [], role: Optional[discord.Role] = None, *, reason = None):
+        """
+        Purges all roles from specified members. Optionally add's a role as well. (Useful for converting members to friends)
+
+        Members can be specified by mention, name, or id.
+        Only one role can be input, however it is not required.
+        The reason can be any text.
+
+        Usage:
+        `?purge Nyx_2#8763 Xyphoer_id_here friend Retired`
+        `?purge my_id_here Xyphoer_id_here Inactive`
+        `?purge my_id_here`
+        """
+        for member in members:
+            member.remove_roles(*member.roles, reason = reason)
+            if role:
+                member.add_roles(role, reason = reason)
+        if members:
+            if role: await ctx.send(f'Purged roles from {member.name for member in members}. Added role {role.name}. Reason: {reason}')
+            else: await ctx.send(f'Purged roles from {member.name for member in members}. Reason: {reason}')
+
+def setup(bot):
+    bot.add_cog(Mod(bot))
