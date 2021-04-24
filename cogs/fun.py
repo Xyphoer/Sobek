7ddec73f69440e4a73bc329c1bcb2b1a809c2c72
Xyphoer <pizzacrime3000@gmail.com>
2021-04-24 16:00:54 -0500

    add dragon check. add fun cog.

---
+++ cogs/fun.py
@@ -0,0 +1,53 @@
+import discord
+from discord.ext import commands
+import random
+import checks
+import asyncio
+
+class Fun(commands.Cog):
+    """Fun commands, generally for enjoyment."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=['profile_picture', 'pic',])
+    async def avatar(self, ctx, member: discord.Member = None):
+        """
+        Displays a members profile picture. Chooses a random member if none are specified.
+
+        Usage:
+        `?avatar`
+        `?avatar 341331627839848448`
+        """
+        if not member:
+            member = random.choice(ctx.guild.members)
+            embed = discord.Embed(description = f'||{member.mention}||')
+        else:
+            embed = discord.Embed(description = f'{member.mention}')
+
+        embed.set_image(url = member.avatar_url)
+        await ctx.send(embed=embed)
+
+    @commands.command(aliases = ['colour'])
+    @checks.is_dragon()
+    async def color(self, ctx, int_code: int = None):
+        """
+        Assigns or updates a personal, 24 hour, color role with the provided color or a random color.
+
+        Usage:
+        `?color`
+        `?color 12648430`
+        """
+        assign_color = int_code if int_code else discord.Colour.random()
+
+        if 'color' not in [role.name for role in ctx.author.roles]:
+            color_role = ctx.guild.create_role(reason = 'color assignment for' + ctx.author.name, name = 'color', color = assign_color)
+            await color_role.edit(position = self.top_role.position - 1)
+            await ctx.author.add_roles(color_role, reason = 'color assignment')
+            await ctx.send(f'Assigned color role with hex code: {hex(color_role.color.value)} int code: {color_role.color.value}')
+            await asyncio.sleep(86400)
+            color_role.delete(reason = 'color role time up.')
+        else:
+            color_role = [role for role in ctx.author.roles if role.name == 'color'][0]
+            await color_role.edit(color = assign_color)
+            await ctx.send(f'Changed color role to hex code: {hex(color_role.color.value)} int code: {color_role.color.value}')
--- cogs/utils/checks.py
+++ cogs/utils/checks.py
@@ -1,5 +1,10 @@
 from discord.ext import commands
 
+def is_dragon():
+    def predicate(ctx):
+        return 444548579839705089 in [role.id for role in ctx.author.roles] or ctx.author.guild_permissions.manage_roles
+    return commands.check(predicate)
+
 def is_commander():
     def predicate(ctx):
         return 446277329300357122 in [role.id for role in ctx.author.roles] or ctx.author.guild_permissions.manage_roles
@@ -7,5 +12,5 @@ def is_commander():
 
 def is_officer():
     def predicate(ctx):
-        return officer_id_here in [role.id for role in ctx.author.roles] or ctx.author.guild_permissions.administrator
+        return 447816270310801439 in [role.id for role in ctx.author.roles] or ctx.author.guild_permissions.administrator
     return commands.check(predicate)
