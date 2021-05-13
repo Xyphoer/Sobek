cd737db5b2499be1936d22f7270663bd1e91ac92
Xyphoer <pizzacrime3000@gmail.com>
2021-05-13 16:02:14 -0500

    moved remind command time converter to formats

--- cogs/general.py
+++ cogs/general.py
@@ -5,6 +5,7 @@ import json
 from asqlite import asqlite
 import re
 from .utils import checks
+from .utils.formats import time_converter
 
 class General(commands.Cog):
     """General commands for use by pretty much anyone."""
@@ -23,39 +24,18 @@ class General(commands.Cog):
             ?remind 2d5h3m some reminder here.
             ?remind 5.8h1d some reminder here.
             ?remind "1d 2h 3m" some reminder here."""
-        regex_type = re.compile(r'd|h|m')
-        regex_amount = re.compile(r'(\d+\.?\d*)|(\.\d+)')
-        style = regex_type.findall(duration.lower())
-        time_full = regex_amount.findall(duration.lower())
-        time = []
-        for y in time_full:
-            for x in y:
-                if x != '':
-                    time.append(x)
-        days, hours, minutes = (0, 0, 0)
-        if len(style) < len(time):
+        value = time_converter(duration)
+        if not value:
             await ctx.send('Invalid time: make sure to format each amount of time with the appropriate letter.')
             return
-        for amount in time.copy():
-            if style[time.index(amount)] == 'd' and days == 0:
-                days = float(amount)
-                time.remove(amount)
-                style.remove('d')
-            elif style[time.index(amount)] == 'h' and hours == 0:
-                hours = float(amount)
-                time.remove(amount)
-                style.remove('h')
-            elif style[time.index(amount)] == 'm' and minutes == 0:
-                minutes = float(amount)
-                time.remove(amount)
-                style.remove('m')
-        total_time = days * 86400 + hours * 3600 + minutes * 60
-        if total_time // 86400 > 10:
+
+        if value['total time'] // 86400 > 10:
             await ctx.send('Max of ten days.')
             return
-        await ctx.send(f'Reminder set for {days}d {hours}h {minutes}m.')
-        await asyncio.sleep(total_time)
-        await ctx.send(f'{ctx.author.mention} {days}d {hours}h {minutes}m ago:\n{content}\n{ctx.message.jump_url}')
+
+        await ctx.send(f'Reminder set for {value["days"]}d {value["hours"]}h {value["minutes"]}m.')
+        await asyncio.sleep(value['total time'])
+        await ctx.send(f'{ctx.author.mention} {value["days"]}d {value["hours"]}h {value["minutes"]}m ago:\n{content}\n{ctx.message.jump_url}')
 
     @commands.command(aliases=['nick'])
     async def nickname(self, ctx, *, nickname):
--- cogs/utils/formats.py
+++ cogs/utils/formats.py
@@ -30,3 +30,34 @@ def one_or_more(matches, container):
             if content == match:
                 return True
     return False
+
+def time_converter(duration):
+    regex_type = re.compile(r'd|h|m')
+    regex_amount = re.compile(r'(\d+\.?\d*)|(\.\d+)')
+    style = regex_type.findall(duration.lower())
+    time_full = regex_amount.findall(duration.lower())
+    time = []
+    value = {}
+    for y in time_full:
+        for x in y:
+            if x != '':
+                time.append(x)
+    value['days'], value['hours'], value['minutes'] = (0, 0, 0)
+    if len(style) < len(time):
+        return False
+    for amount in time.copy():
+        if style[time.index(amount)] == 'd' and days == 0:
+            days = float(amount)
+            time.remove(amount)
+            style.remove('d')
+        elif style[time.index(amount)] == 'h' and hours == 0:
+            hours = float(amount)
+            time.remove(amount)
+            style.remove('h')
+        elif style[time.index(amount)] == 'm' and minutes == 0:
+            minutes = float(amount)
+            time.remove(amount)
+            style.remove('m')
+    value['total time'] = value['days'] * 86400 + value['hours'] * 3600 + value['minutes'] * 60
+
+    return value
